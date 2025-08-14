8f27e33293bd2b6310a32de7703b624a716064dd
TheAliBit <the.ali8bits@gmail.com>
2025-08-14 13:56:22 -0700

    feat: register User, Patient model in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import User, Patient
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ['username', 'created_at', 'updated_at', 'deleted_at', ]
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    pass
