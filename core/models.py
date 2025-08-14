0045fedb73bcaeffbcea394c467ea4e00ea5db54
TheAliBit <the.ali8bits@gmail.com>
2025-08-14 13:30:09 -0700

    refactor: add verbose name for model and fields

--- core/models.py
+++ core/models.py
@@ -5,10 +5,34 @@ from utils.base_models import BaseModel, ID
 
 
 class User(AbstractUser, BaseModel, ID):
-    username = models.CharField(max_length=11, unique=True)
+    username = models.CharField(
+        max_length=11,
+        unique=True,
+        verbose_name="نام کاربری",
+    )
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
 
 
 class Patient(BaseModel, ID):
-    user = models.ForeignKey(to=User, on_delete=models.PROTECT, related_name='patients')
-    full_name = models.CharField(max_length=120)
-    age = models.PositiveIntegerField()
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.PROTECT,
+        related_name='patients',
+        verbose_name="کاربر",
+    )
+
+    full_name = models.CharField(
+        max_length=120,
+        verbose_name="نام کامل",
+    )
+
+    age = models.PositiveIntegerField(
+        verbose_name="سن",
+    )
+
+    class Meta:
+        verbose_name = "بیمار"
+        verbose_name_plural = "بیماران"
